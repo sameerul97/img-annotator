0a1d6917f4222a58bb09198f84152a834af649dd
sameerul97 <sameerul97@gmail.com>
2021-08-08 23:39:07 +0100

    Refactor embed store

--- src/store/Embed/index.tsx
+++ src/store/Embed/index.tsx
@@ -1,7 +1,6 @@
 import React, { createContext, useReducer, Dispatch } from "react";
 import type { ReactNode } from "react";
-import EmbedReducer from "./reducers/embedReducer";
-// import EmbedReducer from "./reducers/_EmbedReducer";
+import EmbedReducer from "./reducers";
 import type { InitialStateType } from "./types";
 import { Action } from "./actions";
 
@@ -20,9 +19,6 @@ const EmbedContext = createContext<{
   state: initialState,
   dispatch: () => undefined
 });
-
-const mainReducer = (state: InitialStateType = initialState, action: Action) =>
-  EmbedReducer(state, action);
 
 const EmbedStoreProvider = ({ children }: { children: ReactNode }) => {
   const [state, dispatch] = useReducer(EmbedReducer, initialState);
